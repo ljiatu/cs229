ccb38a193955a38b4effb4704fcd53c237f971b7
Jiatu Liu <ljiatu@hotmail.com>
2020-10-14 17:46:37 -0700

    Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CS229 Project',
+    version='0.1.0',
+    author='Xinyi Chen, Der-ham Huang, Jiatu Liu',
+    author_email='ljiatu@stanford.edu',
+)
+
