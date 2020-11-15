01292f2700668731eeb114f791f0bc8b906a9abc
Jiatu Liu <ljiatu@hotmail.com>
2020-11-15 14:42:02 -0800

    Run SVR for more MMPs

--- svr.py
+++ svr.py
@@ -9,21 +9,20 @@ from sklearn.svm import SVR
 from utils import load_data
 
 KERNEL_TYPES = ["rbf"]
-REGULARIZATION_STRENGTHS = [16, 32, 64, 128, 256, 512]
-MODEL_SAVE_PATH_FORMAT = "svr_model_mmp_k_{}_c_{}.joblib"
-BEST_MODEL_SAVE_PATH = "svr_model_mmp_best.joblib"
+REGULARIZATION_STRENGTHS = [1, 2, 4, 8, 16, 32]
 
 
 def train(
         train_feature_df: pd.DataFrame,
         train_score_df: pd.DataFrame,
         kernel_type: str,
-        reg_strength: float,
-) -> Tuple[float, SVR]:
+        reg_strength: int,
+        mmp_type: int,
+) -> Tuple[float, SVR, int]:
     """
     Train the model with the given inputs and returns the trained model.
     """
-    print(f"Training SVR model with kernel type {kernel_type} and regularization strength {reg_strength}")
+    print(f"MMP type {mmp_type}. Training SVR model with kernel type {kernel_type} and regularization strength {reg_strength}")
     # Drop the first column, which contains labels for target sequences.
     X = train_feature_df.to_numpy()[:, 1:]
     # y initially comes out as a column vector. Use ravel() to make it a row vector instead.
@@ -39,11 +38,18 @@ def train(
         f"Mean val score: {val_score:0.2f} (+/- {val_scores.std() * 2:0.2f})"
     )
     # Save the individual models.
-    dump(clf, MODEL_SAVE_PATH_FORMAT.format(kernel_type, reg_strength))
-    return val_score, clf
+    dump(clf, f"svr_model_MMP{mmp_type}_k_{kernel_type}_c_{reg_strength}.joblib")
+    return val_score, clf, reg_strength
 
 
-def test(test_feature_df: pd.DataFrame, test_score_df: pd.DataFrame, clf: SVR, test_output_save_path: str) -> None:
+def test(
+        test_feature_df: pd.DataFrame,
+        test_score_df: pd.DataFrame,
+        clf: SVR,
+        test_output_save_path: str,
+        mmp_type: int,
+        reg_strength: int,
+) -> None:
     """
     Predicts on test data, and saves the test output on test_output_save_path.
     """
@@ -56,12 +62,12 @@ def test(test_feature_df: pd.DataFrame, test_score_df: pd.DataFrame, clf: SVR, t
     preds_with_labels.to_csv(test_output_save_path, index=False)
 
     test_score = clf.score(X, y)
-    print(f"Test score: {test_score}")
+    print(f"MMP type: {mmp_type}. Reg strength: {reg_strength}. Test score: {test_score}")
 
 
-def main() -> None:
-    train_feature_df, train_score_df = load_data("data3_MMPs_X_train.csv", "data3_MMP14_y_train.csv")
-    test_feature_df, test_score_df = load_data("data3_MMPs_X_test.csv", "data3_MMP14_y_test.csv")
+def main(mmp_type: int) -> None:
+    train_feature_df, train_score_df = load_data("data3_MMPs_X_train.csv", f"data3_MMP{mmp_type}_y_train.csv")
+    test_feature_df, test_score_df = load_data("data3_MMPs_X_test.csv", f"data3_MMP{mmp_type}_y_test.csv")
 
     pool = multiprocessing.Pool(processes=6)
     train_args = [
@@ -71,16 +77,16 @@ def main() -> None:
     rets = pool.starmap(train, train_args)
 
     best_clf = None
-    best_val_score = float("-inf")
-    for val_score, clf in rets:
-        if val_score > best_val_score:
-            best_val_score = val_score
-            best_clf = clf
+    # best_val_score = float("-inf")
+    for val_score, clf, reg_strength in rets:
+        test(test_feature_df, test_score_df, clf, f"svr_MMP{mmp_type}_preds.csv", mmp_type, reg_strength)
+        # if val_score > best_val_score:
+        # best_val_score = val_score
+        # best_clf = clf
 
-    dump(best_clf, BEST_MODEL_SAVE_PATH)
-    # clf = load("svr_model_best.joblib")
-    test(test_feature_df, test_score_df, best_clf, "svr_MMP14_preds.csv")
+    # dump(best_clf, f"svr_model_MMP{mmp_type}_best.joblib")
 
 
 if __name__ == "__main__":
-    main()
+    for mmp_type in [1, 2, 3, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 19, 20, 24, 25]:
+        main(mmp_type)
